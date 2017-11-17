9c22f12398f0a6933d949057a31d38bc91d66b45
likun1995 <k_888@outlook.com>
2017-11-17 18:15:00 +0800

    Testtt123

--- mybatis_01/src/main/java/cn/bdqn/mybatis_01/App.java
+++ mybatis_01/src/main/java/cn/bdqn/mybatis_01/App.java
@@ -11,6 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hello,git");
         System.out.println("test");
-        System.out.println("branch");System.out.println("123123");
+        System.out.println("branch");System.out.println("123123");System.out.println("123123");
     }
 }
